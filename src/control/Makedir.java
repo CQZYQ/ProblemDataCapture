a58adb5340393023c0d95ea7762f17bef405fc55
CQZYQ <38390005+CQZYQ@users.noreply.github.com>
2018-07-08 14:31:37 +0800

    Updata Makedir
    在以随机生成的id为文件名的文件夹下，再生成in.txt和out.txt两个文件，并写入相应的sample_input和sample_output。

--- src/control/Makedir.java
+++ src/control/Makedir.java
@@ -1,9 +1,12 @@
+
 package control;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Makedir {
-	public boolean makedir(String str) {
+/*	public boolean makedir(String str) {
 		String strDirectory="E:\\SaveCase\\";
 		String filePath=strDirectory+str;
 	File file=new File(filePath);
@@ -11,5 +14,29 @@ public class Makedir {
 		return false;
 	file.mkdirs();
 	return true;
+	}*/
+	public boolean makedir(String str, String incontent, String outcontent) throws IOException {
+		String strDirectory = "E:\\SaveCase\\";
+		String filePath = strDirectory + str;
+		File file = new File(filePath);
+		if (file.exists()) {
+			return false;
+		} else {
+			file.mkdirs();
+			String newfilepathin = filePath + "\\in.txt";
+			String newfilepathout = filePath + "\\out.txt";
+			File filein = new File(newfilepathin);
+			File fileout = new File(newfilepathout);
+			filein.createNewFile();
+			fileout.createNewFile();
+			FileWriter fwin = new FileWriter(newfilepathin, true);
+			fwin.write(incontent);
+			fwin.close();
+			FileWriter fwout = new FileWriter(newfilepathout, true);
+			fwout.write(outcontent);
+			fwout.close();
+			return true;
+		}
 	}
+
 }
